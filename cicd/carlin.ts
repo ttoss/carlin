c89b67de7b9c6e9ac2b616c38d1aecdcb154a628
Pedro Arantes <arantespp@gmail.com>
2021-09-16 15:16:50 -0300

    fix: remove slack webhook

--- cicd/carlin.ts
+++ cicd/carlin.ts
@@ -1,15 +1,14 @@
-import * as fs from 'fs';
+import { secrets } from './secrets';
 
 export default {
   pipelines: ['main', 'tag', 'pr'],
-  slackWebhookUrl:
-    'https://hooks.slack.com/services/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',
+  slackWebhookUrl: secrets.slackWebhookUrl,
   sshKey: './ssh-key',
   sshUrl: 'git@github.com:ttoss/carlin.git',
   taskEnvironment: [
     {
       name: 'NPM_TOKEN',
-      value: fs.readFileSync('./npmtoken', 'utf-8'),
+      value: secrets.npmToken,
     },
   ],
 };
